rchandk4/tensorflow

commit 82a4090a3d50c9820c13c638c574600f916926a8

Add return_nodes option to ImportGraphDef
The is similar to the return_tensors option. return_tensors cannot be
used to fetch nodes with no outputs, so return_nodes is necessary.

In addition, this change also refactors the ImportGraphDef signature
to return all optional return values in a single struct. This is to
keep the ImportGraphDef signature from getting too long, and also
makes the call sites simpler.

PiperOrigin-RevId: 172388270

tensorflow/core/graph/graph_constructor.cc
@@ -79,6 +79,7 @@ class GraphConstructor {
           skip_mapped_nodes(in.skip_mapped_nodes),
           control_dependencies(in.control_dependencies),
           return_tensors(in.return_tensors),
+          return_nodes(in.return_nodes),
           importing(true) {}
 
     bool allow_internal_ops;
@@ -89,6 +90,7 @@ class GraphConstructor {
     bool skip_mapped_nodes;
     std::vector<string> control_dependencies;
     std::vector<TensorId> return_tensors;
+    std::vector<StringPiece> return_nodes;
 
     // TODO(ashankar): This bool exists to separate out functionality required
     // to make ImportGraphDef a close equivalent of Python's import_graph_def
@@ -109,6 +111,7 @@ class GraphConstructor {
                           const FunctionDefLibrary* library, Graph* g,
                           ShapeRefiner* refiner,
                           std::vector<std::pair<Node*, int>>* return_tensors,
+                          std::vector<Node*>* return_nodes,
                           std::vector<TensorId>* unused_input_map_keys) {
     if (versions) {
       TF_RETURN_IF_ERROR(CheckVersions(*versions, TF_GRAPH_DEF_VERSION,
@@ -116,7 +119,7 @@ class GraphConstructor {
                                        "GraphDef", "graph"));
     }
     GraphConstructor c(opts, node_defs, versions, library, g, refiner,
-                       return_tensors, unused_input_map_keys);
+                       return_tensors, return_nodes, unused_input_map_keys);
     const Status s = c.TryImport();
     if (!s.ok()) c.Undo();
     return s;
@@ -128,6 +131,7 @@ class GraphConstructor {
                    const FunctionDefLibrary* library, Graph* g,
                    ShapeRefiner* refiner,
                    std::vector<std::pair<Node*, int>>* return_tensors,
+                   std::vector<Node*>* return_nodes,
                    std::vector<TensorId>* unused_input_map_keys)
       : opts_(opts),
         node_defs_(node_defs),
@@ -137,6 +141,7 @@ class GraphConstructor {
         original_versions_(g->versions()),
         refiner_(refiner),
         return_tensors_(return_tensors),
+        return_nodes_(return_nodes),
         unused_input_map_keys_(unused_input_map_keys) {}
 
   Status TryImport() {
@@ -148,6 +153,7 @@ class GraphConstructor {
     TF_RETURN_IF_ERROR(AddBackEdges());
     TF_RETURN_IF_ERROR(UpdateVersionDef());
     TF_RETURN_IF_ERROR(PopulateReturnTensors());
+    TF_RETURN_IF_ERROR(PopulateReturnNodes());
     FixupSourceAndSinkEdges(g_);
     return Status::OK();
   }
@@ -160,6 +166,7 @@ class GraphConstructor {
   Status AddBackEdges();
   Status UpdateVersionDef();
   Status PopulateReturnTensors();
+  Status PopulateReturnNodes();
 
   void Undo();
 
@@ -195,6 +202,9 @@ class GraphConstructor {
 
   // May be null. Not owned.
   std::vector<std::pair<Node*, int>>* return_tensors_;
+
+  // May be null. Not owned.
+  std::vector<Node*>* return_nodes_;
 
   // May be null. Not owned.
   std::vector<TensorId>* unused_input_map_keys_;
@@ -913,7 +923,8 @@ Status GraphConstructor::PopulateReturnTensors() {
       // Locate id in imported nodes
       auto iter = gdef_nodes_.find(id.first);
       if (iter == gdef_nodes_.end()) {
-        return errors::InvalidArgument("Requested return node '", id.first,
+        return errors::InvalidArgument("Requested return tensor '",
+                                       id.ToString(),
                                        "' not found in graph def");
       }
       int num_outputs = iter->second.node->num_outputs();
@@ -931,6 +942,19 @@ Status GraphConstructor::PopulateReturnTensors() {
       Node* node = existing_nodes_[remapped_id.first];
       return_tensors_->push_back({node, remapped_id.second});
     }
+  }
+  return Status::OK();
+}
+
+Status GraphConstructor::PopulateReturnNodes() {
+  if (opts_.return_nodes.empty()) return Status::OK();
+  for (StringPiece name : opts_.return_nodes) {
+    auto iter = gdef_nodes_.find(name);
+    if (iter == gdef_nodes_.end()) {
+      return errors::InvalidArgument("Requested return node '", name,
+                                     "' not found in graph def");
+    }
+    return_nodes_->push_back(iter->second.node);
   }
   return Status::OK();
 }
@@ -965,7 +989,8 @@ Status ConvertGraphDefToGraph(const GraphConstructorOptions& opts,
   ShapeRefiner refiner(gdef.versions().producer(), g->op_registry());
   return GraphConstructor::Construct(
       opts, gdef.node(), &gdef.versions(), &gdef.library(), g, &refiner,
-      /*return_tensors=*/nullptr, /*unused_input_map_keys=*/nullptr);
+      /*return_tensors=*/nullptr, /*return_nodes=*/nullptr,
+      /*unused_input_map_keys=*/nullptr);
 }
 
 Status ConvertNodeDefsToGraph(const GraphConstructorOptions& opts,
@@ -978,31 +1003,40 @@ Status ConvertNodeDefsToGraph(const GraphConstructorOptions& opts,
   }
   return GraphConstructor::Construct(opts, node_defs, nullptr, nullptr, g,
                                      &refiner, /*return_tensors=*/nullptr,
+                                     /*return_nodes=*/nullptr,
                                      /*unused_input_map_keys=*/nullptr);
 }
 
 Status ImportGraphDef(const ImportGraphDefOptions& opts, const GraphDef& gdef,
                       Graph* g, ShapeRefiner* refiner,
-                      std::vector<std::pair<Node*, int>>* return_tensors,
-                      std::vector<TensorId>* unused_input_map_keys) {
+                      ImportGraphDefResults* results) {
   if (!opts.return_tensors.empty()) {
-    if (return_tensors == nullptr) {
+    if (results == nullptr) {
       return errors::InvalidArgument(
-          "return_tensors argument to ImportGraphDef() must be non-null if "
+          "results argument to ImportGraphDef() must be non-null if "
           "opts.return_tensors is non-empty");
     }
-    if (!return_tensors->empty()) {
+  }
+
+  if (!opts.return_nodes.empty()) {
+    if (opts.skip_mapped_nodes) {
       return errors::InvalidArgument(
-          "return_tensors argument to ImportGraphDef() should be empty (has "
-          "size ",
-          return_tensors->size(), ")");
+          "Requesting return_nodes with skip_mapped_nodes set is not currently "
+          "supported");
+    }
+    if (results == nullptr) {
+      return errors::InvalidArgument(
+          "results argument to ImportGraphDef() must be non-null if "
+          "opts.return_nodes is non-empty");
     }
   }
-  if (unused_input_map_keys != nullptr && !unused_input_map_keys->empty()) {
-    return errors::InvalidArgument(
-        "If non-null, unused_input_map_keys argument to ImportGraphDef() should"
-        " be empty (has size ",
-        unused_input_map_keys->size(), ")");
+
+  if (results != nullptr) {
+    if (!results->return_tensors.empty() || !results->return_nodes.empty() ||
+        !results->unused_input_map_keys.empty()) {
+      return errors::InvalidArgument(
+          "All fields in results argument to ImportGraphDef() must be empty.");
+    }
   }
 
   ShapeRefiner default_refiner(gdef.versions().producer(), g->op_registry());
@@ -1034,9 +1068,10 @@ Status ImportGraphDef(const ImportGraphDefOptions& opts, const GraphDef& gdef,
   refiner->set_graph_def_version(
       std::min(refiner->graph_def_version(), gdef.versions().producer()));
 
-  return GraphConstructor::Construct(opts, gdef.node(), &gdef.versions(),
-                                     &gdef.library(), g, refiner,
-                                     return_tensors, unused_input_map_keys);
+  return GraphConstructor::Construct(
+      opts, gdef.node(), &gdef.versions(), &gdef.library(), g, refiner,
+      &results->return_tensors, &results->return_nodes,
+      &results->unused_input_map_keys);
 }
 
 void CopyGraph(const Graph& src, Graph* dest) {

tensorflow/c/c_api.cc
@@ -1854,18 +1854,18 @@ static void GraphImportGraphDefLocked(TF_Graph* graph, const GraphDef& def,
     return;
   }
   const int last_node_id = graph->graph.num_node_ids();
-  std::vector<std::pair<Node*, int>> return_outputs_vec;
-  status->status = tensorflow::ImportGraphDef(
-      opts->opts, def, &graph->graph, &graph->refiner, &return_outputs_vec);
+  tensorflow::ImportGraphDefResults results;
+  status->status = tensorflow::ImportGraphDef(opts->opts, def, &graph->graph,
+                                              &graph->refiner, &results);
   if (!status->status.ok()) return;
   for (int i = last_node_id; i < graph->graph.num_node_ids(); ++i) {
     auto* node = graph->graph.FindNodeId(i);
     if (node != nullptr) graph->name_map[node->name()] = node;
   }
-  DCHECK_EQ(return_outputs_vec.size(), num_return_outputs);
+  DCHECK_EQ(results.return_tensors.size(), num_return_outputs);
   for (int i = 0; i < num_return_outputs; ++i) {
-    return_outputs[i].oper = ToOperation(return_outputs_vec[i].first);
-    return_outputs[i].index = return_outputs_vec[i].second;
+    return_outputs[i].oper = ToOperation(results.return_tensors[i].first);
+    return_outputs[i].index = results.return_tensors[i].second;
   }
 }
 
@@ -1945,11 +1945,11 @@ Status CopyGraph(Graph* src_graph, Graph* dst_graph,
   }
 
   // TOOD(skyewm): change to OutputTensor
-  std::vector<std::pair<Node*, int>> return_tensors;
+  tensorflow::ImportGraphDefResults results;
   TF_RETURN_IF_ERROR(
-      ImportGraphDef(opts, gdef, dst_graph, dst_refiner, &return_tensors));
+      ImportGraphDef(opts, gdef, dst_graph, dst_refiner, &results));
 
-  for (const auto& pair : return_tensors) {
+  for (const auto& pair : results.return_tensors) {
     return_nodes->emplace_back(pair.first, pair.second);
   }
   return Status::OK();

tensorflow/c/while_loop_test.cc
@@ -318,7 +318,7 @@ TEST_F(CApiWhileLoopTest, InvalidCondOutputNode) {
   // TODO(skyewm): this error message could be more informative. Add explicit
   // checks for this case in the while loop implementation?
   ExpectError(TF_INVALID_ARGUMENT,
-              "Requested return node 'p0' not found in graph def");
+              "Requested return tensor 'p0:0' not found in graph def");
 }
 
 TEST_F(CApiWhileLoopTest, InvalidCondOutputIndex) {
@@ -358,7 +358,7 @@ TEST_F(CApiWhileLoopTest, InvalidBodyOutputNode) {
   // TODO(skyewm): this error message could be more informative. Add explicit
   // checks for this case in the while loop implementation?
   ExpectError(TF_INVALID_ARGUMENT,
-              "Requested return node 'p0' not found in graph def");
+              "Requested return tensor 'p0:0' not found in graph def");
 }
 
 // TODO(skyewm): enable this when it works (currently segfaults!)
@@ -389,7 +389,7 @@ TEST_F(CApiWhileLoopTest, WrongGraph) {
   params_->body_outputs[0] = inputs_[0];
   // TODO(skyewm): improve error message
   ExpectError(TF_INVALID_ARGUMENT,
-              "Requested return node 'p0' not found in graph def");
+              "Requested return tensor 'p0:0' not found in graph def");
 }
 
 TEST_F(CApiWhileLoopTest, BadTypes) {

tensorflow/core/graph/graph_constructor.h
@@ -72,8 +72,6 @@ struct ImportGraphDefOptions {
   // used to create the existing nodes referenced in `input_map`.
   // TODO(skyewm): can we remove this requirement? How do we access the original
   // shape refiner?
-  //
-  // TODO(skyewm): add functionality to retrieve unused `input_map` keys
   std::map<TensorId, TensorId> input_map;
 
   // If true, nodes that will have all output edges removed because of
@@ -88,10 +86,10 @@ struct ImportGraphDefOptions {
   // other nodes in `gdef`.
   std::vector<string> control_dependencies;
 
-  // Tensors in `gdef` that will be returned via the `return_tensors` output
-  // parameter of `ImportGraphDef()`. If this list is non-empty, the caller must
-  // pass an empty vector to `ImportGraphDef()`. The vector will be populated
-  // with the imported nodes in `g`.
+  // Tensors in `gdef` that will be returned via the ImportGraphDefResults
+  // output parameter of `ImportGraphDef()`. If this list is non-empty, the
+  // caller must pass a results object to `ImportGraphDef()`. The
+  // `return_tensors` field will be populated with the imported nodes in `g`.
   //
   // Entries should not include `prefix`, i.e., each TensorId's name should be
   // the name as it originally appears in `gdef`.
@@ -100,10 +98,41 @@ struct ImportGraphDefOptions {
   // corresponding existing tensor in `g` will be returned.
   std::vector<TensorId> return_tensors;
 
+  // The names of nodes in `gdef` that will be returned via the
+  // ImportGraphDefResults output parameter of `ImportGraphDef()`. If this list
+  // is non-empty, the caller must pass a results object to
+  // `ImportGraphDef()`. The `return_nodes` field will be populated with the
+  // imported nodes in `g`.
+  //
+  // Entries should not include `prefix`, i.e., each node's name should be the
+  // name as it originally appears in `gdef`.
+  //
+  // Unlike `return_tensors`, `input_map` has no effect on the nodes
+  // returned. `return_nodes` must be empty if `skip_mapped_nodes` is true.
+  // TODO(skyewm): make this work with `skip_mapped_nodes` if there's a need.
+  std::vector<StringPiece> return_nodes;
+
   // TODO(ashankar): Enable handling of GraphDefs produced by newer binaries
   // with ops that are not defined in the binary calling ImportGraphDef.
   // Similar to the producer_op_list argument to import_graph_def in the
   // python API.
+};
+
+// Optional results that may be returned by ImportGraphDef.
+struct ImportGraphDefResults {
+  // The requested tensors associated with
+  // ImportGraphDefOptions::return_tensors. Note that the index may be different
+  // than the requested index if the returned tensor has been remapped according
+  // to `input_map`.
+  typedef int Index;
+  std::vector<std::pair<Node*, Index>> return_tensors;
+
+  // The requested nodes associated with ImportGraphDefOptions::return_nodes.
+  std::vector<Node*> return_nodes;
+
+  // Keys in ImportGraphDefOptions::input_map that weren't used as an input to
+  // any node in`gdef`.
+  std::vector<TensorId> unused_input_map_keys;
 };
 
 // Adds the graph in GraphDef `gdef` into an existing Graph `*g`.
@@ -115,21 +144,16 @@ struct ImportGraphDefOptions {
 // allows the caller to validate shapes of those nodes (since
 // ShapeRefiner::AddNode must be called in topological order).
 //
-// Each `return_tensors` entry is the requested node and output index. The index
-// is included in case the returned tensor has been remapped according to
-// `input_map`.
-//
-// If `unused_input_map_keys` is non-null, it should be empty and will be
-// populated with any keys in `opts.input_map` that aren't used as an input to
-// any node in `gdef`.
+// `results` must be non-null if `opts.return_tensors` or `opts.result_nodes` is
+// non-empty. It can also be set to fetch the unused input map keys. If it's
+// non-null, all the vector fields must be empty.
 //
 // TODO(ashankar): Push this mechanism and get rid of Session::Extend()
 // as a means of enhancing an existing Graph.
-extern Status ImportGraphDef(
-    const ImportGraphDefOptions& opts, const GraphDef& gdef, Graph* g,
-    ShapeRefiner* refiner,
-    std::vector<std::pair<Node*, int>>* return_tensors = nullptr,
-    std::vector<TensorId>* unused_input_map_keys = nullptr);
+extern Status ImportGraphDef(const ImportGraphDefOptions& opts,
+                             const GraphDef& gdef, Graph* g,
+                             ShapeRefiner* refiner,
+                             ImportGraphDefResults* results = nullptr);
 
 // Make a copy of "src" into "*dest".
 //

commit 4eebf1ad11499e538283fa77303a2c5688d2895b

Fix bug introduced in https://github.com/tensorflow/tensorflow/commit/82a4090a3d50c9820c13c638c574600f916926a8
Potentially invalid pointers passed to GraphConstructor::Construct()

PiperOrigin-RevId: 172826567

tensorflow/core/graph/graph_constructor.cc
@@ -1068,10 +1068,16 @@ Status ImportGraphDef(const ImportGraphDefOptions& opts, const GraphDef& gdef,
   refiner->set_graph_def_version(
       std::min(refiner->graph_def_version(), gdef.versions().producer()));
 
-  return GraphConstructor::Construct(
-      opts, gdef.node(), &gdef.versions(), &gdef.library(), g, refiner,
-      &results->return_tensors, &results->return_nodes,
-      &results->unused_input_map_keys);
+  if (results == nullptr) {
+    return GraphConstructor::Construct(opts, gdef.node(), &gdef.versions(),
+                                       &gdef.library(), g, refiner, nullptr,
+                                       nullptr, nullptr);
+  } else {
+    return GraphConstructor::Construct(
+        opts, gdef.node(), &gdef.versions(), &gdef.library(), g, refiner,
+        &results->return_tensors, &results->return_nodes,
+        &results->unused_input_map_keys);
+  }
 }
 
 void CopyGraph(const Graph& src, Graph* dest) {