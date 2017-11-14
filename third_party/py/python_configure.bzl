rchandk4/tensorflow

commit 8c949d9d598c018c37c120a781219ee4e4673ebc

Clean up python configuration: - Remove experimental attributes (local_checks, python_include, numpy_include, remote_config_repo). Use of experimental remote repo can only be enabled now via env variable (preferred to change to workspace file) - Fix hardcoded fallback value for python bin. New fallback is to use 'which python' and fail if python not set in path (solves tensorflow/tensorflow#9436 / works around bazelbuild/bazel#3057). - Fixes minor issues with template: adds spaces to srcs in genrule, remove extra space after genrules. - Some dead code clean up (use of empty configuration)
PiperOrigin-RevId: 160594421

third_party/py/python_configure.bzl
@@ -9,9 +9,7 @@
   * `PYTHON_LIB_PATH`: Location of python libraries.
 """
 
-_NUMPY_INCLUDE_PATH = "NUMPY_INCLUDE_PATH"
 _PYTHON_BIN_PATH = "PYTHON_BIN_PATH"
-_PYTHON_INCLUDE_PATH = "PYTHON_INCLUDE_PATH"
 _PYTHON_LIB_PATH = "PYTHON_LIB_PATH"
 _TF_PYTHON_CONFIG_REPO = "TF_PYTHON_CONFIG_REPO"
 
@@ -121,7 +119,7 @@ def _genrule(src_dir, genrule_name, command, outs):
       '    cmd = """\n' +
       command +
       '\n   """,\n' +
-      ')\n\n'
+      ')\n'
   )
 
 
@@ -157,10 +155,25 @@ def _symlink_genrule_for_dir(repository_ctx, src_dir, dest_dir, genrule_name,
       # On Windows, symlink is not supported, so we just copy all the files.
       cmd = 'cp -f' if _is_windows(repository_ctx) else 'ln -s'
       command.append(cmd + ' "%s" "%s"' % (src_files[i] , dest))
-      outs.append('      "' + dest_dir + dest_files[i] + '",')
+      outs.append('        "' + dest_dir + dest_files[i] + '",')
   genrule = _genrule(src_dir, genrule_name, " && ".join(command),
                      "\n".join(outs))
   return genrule
+
+
+def _get_python_bin(repository_ctx):
+  """Gets the python bin path."""
+  python_bin = _get_env_var(repository_ctx, _PYTHON_BIN_PATH,
+                            None, False)
+  if python_bin != None:
+    return python_bin
+  python_bin_path = repository_ctx.which("python")
+  if python_bin_path != None:
+    return str(python_bin_path)
+  path = _get_env_var(repository_ctx, "PATH")
+  _python_configure_fail("Cannot find python in PATH, please make sure " +
+      "python is installed and add its directory in PATH, or set the " +
+      "environment variable PYTHON_BIN_PATH.\nPATH=%s" % (path))
 
 
 def _get_python_lib(repository_ctx, python_bin):
@@ -252,63 +265,32 @@ def _get_numpy_include(repository_ctx, python_bin):
 
 def _create_local_python_repository(repository_ctx):
   """Creates the repository containing files set up to build with Python."""
-  python_include = None
-  numpy_include = None
-  empty_config = False
-  # If local checks were requested, the python and numpy include will be auto
-  # detected on the host config (using _PYTHON_BIN_PATH).
-  python_bin = _get_env_var(repository_ctx, _PYTHON_BIN_PATH,
-                            "/usr/bin/python")
-  if repository_ctx.attr.local_checks:
-    # TODO(nlopezgi): The default argument here is a workaround until
-    #                 bazelbuild/bazel#3057 is resolved.
-    _check_python_bin(repository_ctx, python_bin)
-    python_lib = _get_env_var(repository_ctx, _PYTHON_LIB_PATH,
+  python_bin = _get_python_bin(repository_ctx)
+  _check_python_bin(repository_ctx, python_bin)
+  python_lib = _get_env_var(repository_ctx, _PYTHON_LIB_PATH,
                               _get_python_lib(repository_ctx, python_bin))
-    _check_python_lib(repository_ctx, python_lib)
-    python_include = _get_python_include(repository_ctx, python_bin)
-    numpy_include = _get_numpy_include(repository_ctx, python_bin) + '/numpy'
-  else:
-    # Otherwise, we assume user provides all paths (via ENV or attrs)
-    python_include = _get_env_var(repository_ctx, _PYTHON_INCLUDE_PATH,
-                                  repository_ctx.attr.python_include)
-    numpy_include = _get_env_var(repository_ctx, _NUMPY_INCLUDE_PATH,
-                                 repository_ctx.attr.numpy_include) + '/numpy'
-  if empty_config:
-    _tpl(repository_ctx, "BUILD", {
-        "%{PYTHON_INCLUDE_GENRULE}": ('filegroup(\n' +
-                                      '    name = "python_include",\n' +
-                                      '    srcs = [],\n' +
-                                      ')\n'),
-        "%{PYTHON_IMPORT_LIB_GENRULE}": ('filegroup(\n' +
-                                      '    name = "python_import_lib",\n' +
-                                      '    srcs = [],\n' +
-                                      ')\n'),
-        "%{NUMPY_INCLUDE_GENRULE}": ('filegroup(\n' +
-                                      '    name = "numpy_include",\n' +
-                                      '    srcs = [],\n' +
-                                      ')\n'),
-    })
-  else:
-    python_include_rule = _symlink_genrule_for_dir(
-        repository_ctx, python_include, 'python_include', 'python_include')
-    python_import_lib_genrule = ""
-    # To build Python C/C++ extension on Windows, we need to link to python import library pythonXY.lib
-    # See https://docs.python.org/3/extending/windows.html
-    if _is_windows(repository_ctx):
-      python_include = _norm_path(python_include)
-      python_import_lib_name = _get_python_import_lib_name(repository_ctx, python_bin)
-      python_import_lib_src = python_include.rsplit('/', 1)[0] + "/libs/" + python_import_lib_name
-      python_import_lib_genrule = _symlink_genrule_for_dir(
-        repository_ctx, None, '', 'python_import_lib',
-        [python_import_lib_src], [python_import_lib_name])
-    numpy_include_rule = _symlink_genrule_for_dir(
-        repository_ctx, numpy_include, 'numpy_include/numpy', 'numpy_include')
-    _tpl(repository_ctx, "BUILD", {
-        "%{PYTHON_INCLUDE_GENRULE}": python_include_rule,
-        "%{PYTHON_IMPORT_LIB_GENRULE}": python_import_lib_genrule,
-        "%{NUMPY_INCLUDE_GENRULE}": numpy_include_rule,
-    })
+  _check_python_lib(repository_ctx, python_lib)
+  python_include = _get_python_include(repository_ctx, python_bin)
+  numpy_include = _get_numpy_include(repository_ctx, python_bin) + '/numpy'
+  python_include_rule = _symlink_genrule_for_dir(
+      repository_ctx, python_include, 'python_include', 'python_include')
+  python_import_lib_genrule = ""
+  # To build Python C/C++ extension on Windows, we need to link to python import library pythonXY.lib
+  # See https://docs.python.org/3/extending/windows.html
+  if _is_windows(repository_ctx):
+    python_include = _norm_path(python_include)
+    python_import_lib_name = _get_python_import_lib_name(repository_ctx, python_bin)
+    python_import_lib_src = python_include.rsplit('/', 1)[0] + "/libs/" + python_import_lib_name
+    python_import_lib_genrule = _symlink_genrule_for_dir(
+      repository_ctx, None, '', 'python_import_lib',
+      [python_import_lib_src], [python_import_lib_name])
+  numpy_include_rule = _symlink_genrule_for_dir(
+      repository_ctx, numpy_include, 'numpy_include/numpy', 'numpy_include')
+  _tpl(repository_ctx, "BUILD", {
+      "%{PYTHON_INCLUDE_GENRULE}": python_include_rule,
+      "%{PYTHON_IMPORT_LIB_GENRULE}": python_import_lib_genrule,
+      "%{NUMPY_INCLUDE_GENRULE}": numpy_include_rule,
+  })
 
 
 def _create_remote_python_repository(repository_ctx, remote_config_repo):
@@ -321,27 +303,18 @@ def _create_remote_python_repository(repository_ctx, remote_config_repo):
 
 def _python_autoconf_impl(repository_ctx):
   """Implementation of the python_autoconf repository rule."""
-  remote_config_repo = _get_env_var(repository_ctx, _TF_PYTHON_CONFIG_REPO,
-      repository_ctx.attr.remote_config_repo, False)
-  if remote_config_repo != "":
-    _create_remote_python_repository(repository_ctx, remote_config_repo)
+  if _TF_PYTHON_CONFIG_REPO in repository_ctx.os.environ:
+      _create_remote_python_repository(repository_ctx,
+          repository_ctx.os.environ[_TF_PYTHON_CONFIG_REPO])
   else:
     _create_local_python_repository(repository_ctx)
 
 
 python_configure = repository_rule(
     implementation = _python_autoconf_impl,
-    attrs = {
-        "local_checks": attr.bool(mandatory = False, default = True),
-        "python_include": attr.string(mandatory = False),
-        "numpy_include": attr.string(mandatory = False),
-        "remote_config_repo": attr.string(mandatory = False, default =""),
-    },
     environ = [
         _PYTHON_BIN_PATH,
-        _PYTHON_INCLUDE_PATH,
         _PYTHON_LIB_PATH,
-        _NUMPY_INCLUDE_PATH,
         _TF_PYTHON_CONFIG_REPO,
     ],
 )

third_party/py/BUILD.tpl
@@ -31,7 +31,5 @@ config_setting(
 )
 
 %{PYTHON_INCLUDE_GENRULE}
-
 %{PYTHON_IMPORT_LIB_GENRULE}
-
 %{NUMPY_INCLUDE_GENRULE}

commit a929a5a54f4c500f768147bca4d2287b0142c0eb

Fix Python auto configure build support (#14467)
8c949d9 caused our Python autoconf to no longer be auto. This was likely due to
the evil helper _get_env_var() which has now been removed.

This change blocks tensorflow/tensorboard#719

third_party/py/python_configure.bzl
@@ -1,11 +1,8 @@
-# -*- Python -*-
 """Repository rule for Python autoconfiguration.
 
 `python_configure` depends on the following environment variables:
 
-  * `NUMPY_INCLUDE_PATH`: Location of Numpy libraries.
   * `PYTHON_BIN_PATH`: location of python binary.
-  * `PYTHON_INCLUDE_PATH`: Location of python binaries.
   * `PYTHON_LIB_PATH`: Location of python libraries.
 """
 
@@ -23,30 +20,11 @@ def _tpl(repository_ctx, tpl, substitutions={}, out=None):
       substitutions)
 
 
-def _python_configure_warning(msg):
-  """Output warning message during auto configuration."""
-  yellow = "\033[1;33m"
-  no_color = "\033[0m"
-  print("%sPython Configuration Warning:%s %s" % (yellow, no_color, msg))
-
-
-def _python_configure_fail(msg):
+def _fail(msg):
   """Output failure message when auto configuration fails."""
   red = "\033[0;31m"
   no_color = "\033[0m"
   fail("%sPython Configuration Error:%s %s\n" % (red, no_color, msg))
-
-
-def _get_env_var(repository_ctx, name, default = None, enable_warning = True):
-  """Find an environment variable in system path."""
-  if name in repository_ctx.os.environ:
-    return repository_ctx.os.environ[name]
-  if default != None:
-    if enable_warning:
-      _python_configure_warning(
-          "'%s' environment variable is not set, using '%s' as default" % (name, default))
-    return default
-  _python_configure_fail("'%s' environment variable is not set" % name)
 
 
 def _is_windows(repository_ctx):
@@ -73,11 +51,10 @@ def _execute(repository_ctx, cmdline, error_msg=None, error_details=None,
   """
   result = repository_ctx.execute(cmdline)
   if result.stderr or not (empty_stdout_fine or result.stdout):
-    _python_configure_fail(
-        "\n".join([
-            error_msg.strip() if error_msg else "Repository command failed",
-            result.stderr.strip(),
-            error_details if error_details else ""]))
+    _fail("\n".join([
+        error_msg.strip() if error_msg else "Repository command failed",
+        result.stderr.strip(),
+        error_details if error_details else ""]))
   return result
 
 
@@ -163,21 +140,23 @@ def _symlink_genrule_for_dir(repository_ctx, src_dir, dest_dir, genrule_name,
 
 def _get_python_bin(repository_ctx):
   """Gets the python bin path."""
-  python_bin = _get_env_var(repository_ctx, _PYTHON_BIN_PATH,
-                            None, False)
+  python_bin = repository_ctx.os.environ.get(_PYTHON_BIN_PATH)
   if python_bin != None:
     return python_bin
   python_bin_path = repository_ctx.which("python")
   if python_bin_path != None:
     return str(python_bin_path)
-  path = _get_env_var(repository_ctx, "PATH")
-  _python_configure_fail("Cannot find python in PATH, please make sure " +
-      "python is installed and add its directory in PATH, or set the " +
-      "environment variable PYTHON_BIN_PATH.\nPATH=%s" % (path))
+  _fail("Cannot find python in PATH, please make sure " +
+        "python is installed and add its directory in PATH, or --define " +
+        "%s='/something/else'.\nPATH=%s" % (
+            _PYTHON_BIN_PATH, repository_ctx.os.environ.get("PATH", "")))
 
 
 def _get_python_lib(repository_ctx, python_bin):
   """Gets the python lib path."""
+  python_lib = repository_ctx.os.environ.get(_PYTHON_LIB_PATH)
+  if python_lib != None:
+    return python_lib
   print_lib = ("<<END\n" +
       "from __future__ import print_function\n" +
       "import site\n" +
@@ -214,7 +193,7 @@ def _check_python_lib(repository_ctx, python_lib):
   cmd = 'test -d "%s" -a -x "%s"' % (python_lib, python_lib)
   result = repository_ctx.execute(["bash", "-c", cmd])
   if result.return_code == 1:
-    _python_configure_fail("Invalid python library path:  %s" % python_lib)
+    _fail("Invalid python library path: %s" % python_lib)
 
 
 def _check_python_bin(repository_ctx, python_bin):
@@ -222,33 +201,36 @@ def _check_python_bin(repository_ctx, python_bin):
   cmd =  '[[ -x "%s" ]] && [[ ! -d "%s" ]]' % (python_bin, python_bin)
   result = repository_ctx.execute(["bash", "-c", cmd])
   if result.return_code == 1:
-    _python_configure_fail(
-        "PYTHON_BIN_PATH is not executable.  Is it the python binary?")
+    _fail("--define %s='%s' is not executable. Is it the python binary?" % (
+        _PYTHON_BIN_PATH, python_bin))
 
 
 def _get_python_include(repository_ctx, python_bin):
   """Gets the python include path."""
-  result = _execute(repository_ctx,
-                    [python_bin, "-c",
-                     'from __future__ import print_function;' +
-                     'from distutils import sysconfig;' +
-                     'print(sysconfig.get_python_inc())'],
-                    error_msg="Problem getting python include path.",
-                    error_details=("Is the Python binary path set up right? " +
-                                   "(See ./configure or PYTHON_BIN_PATH.) " +
-                                   "Is distutils installed?"))
+  result = _execute(
+      repository_ctx,
+      [python_bin, "-c",
+       'from __future__ import print_function;' +
+       'from distutils import sysconfig;' +
+       'print(sysconfig.get_python_inc())'],
+      error_msg="Problem getting python include path.",
+      error_details=("Is the Python binary path set up right? " +
+                     "(See ./configure or " + _PYTHON_BIN_PATH + ".) " +
+                     "Is distutils installed?"))
   return result.stdout.splitlines()[0]
 
 
 def _get_python_import_lib_name(repository_ctx, python_bin):
   """Get Python import library name (pythonXY.lib) on Windows."""
-  result = _execute(repository_ctx,
-                    [python_bin, "-c",
-                     'import sys;' +
-                     'print("python" + str(sys.version_info[0]) + str(sys.version_info[1]) + ".lib")'],
-                    error_msg="Problem getting python import library.",
-                    error_details=("Is the Python binary path set up right? " +
-                                   "(See ./configure or PYTHON_BIN_PATH.) "))
+  result = _execute(
+      repository_ctx,
+      [python_bin, "-c",
+       'import sys;' +
+       'print("python" + str(sys.version_info[0]) + ' +
+       '      str(sys.version_info[1]) + ".lib")'],
+      error_msg="Problem getting python import library.",
+      error_details=("Is the Python binary path set up right? " +
+                     "(See ./configure or " + _PYTHON_BIN_PATH + ".) "))
   return result.stdout.splitlines()[0]
 
 
@@ -267,8 +249,7 @@ def _create_local_python_repository(repository_ctx):
   """Creates the repository containing files set up to build with Python."""
   python_bin = _get_python_bin(repository_ctx)
   _check_python_bin(repository_ctx, python_bin)
-  python_lib = _get_env_var(repository_ctx, _PYTHON_LIB_PATH,
-                              _get_python_lib(repository_ctx, python_bin))
+  python_lib = _get_python_lib(repository_ctx, python_bin)
   _check_python_lib(repository_ctx, python_lib)
   python_include = _get_python_include(repository_ctx, python_bin)
   numpy_include = _get_numpy_include(repository_ctx, python_bin) + '/numpy'